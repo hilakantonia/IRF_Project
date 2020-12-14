59b58bb4fe61e704fd9f869f42458ebb54e4309e
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-14 23:44:23 +0100

    Datagridview feltöltése adatokkal

--- Beadando_sp2p8b/Beadando_sp2p8b/Form1.cs
+++ Beadando_sp2p8b/Beadando_sp2p8b/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,29 @@ namespace Beadando_sp2p8b
         public Form1()
         {
             InitializeComponent();
+            LoadMovies();
+
+            dataGridView1.DataSource = _movies;
+
+        }
+
+        private List<Movie> _movies = new List<Movie>();
+
+        private void LoadMovies()
+        {
+            _movies.Clear();
+            using (StreamReader sr = new StreamReader("IMDbmovies.csv", Encoding.Default))
+            {
+                sr.ReadLine(); // Remove headers
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+
+                    Movie s = new Movie();
+                    s.Title = line[1];
+                    _movies.Add(s);
+                }
+            }
         }
     }
 }
