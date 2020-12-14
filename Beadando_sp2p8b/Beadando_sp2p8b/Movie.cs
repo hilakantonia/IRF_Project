01aaf86b4792e18c9f434b7aef047bb08580595a
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-14 23:26:55 +0100

    Movie osztály

---
+++ Beadando_sp2p8b/Beadando_sp2p8b/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando_sp2p8b
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public string Language { get; set; }
+        public string Director { get; set; }
+        public int Review { get; set; }
+
+
+    }
+}
