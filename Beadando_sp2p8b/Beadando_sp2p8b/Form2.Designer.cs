15ae41a5db11b65c030d370b7ea38e297a36f236
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-16 22:27:56 +0100

    Form 2 Design

--- Beadando_sp2p8b/Beadando_sp2p8b/Form2.Designer.cs
+++ Beadando_sp2p8b/Beadando_sp2p8b/Form2.Designer.cs
@@ -41,15 +41,17 @@ namespace Beadando_sp2p8b
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 66);
+            this.label1.BackColor = System.Drawing.Color.LightYellow;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(27, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(72, 27);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "E-mail:";
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(105, 63);
+            this.txtEmail.Location = new System.Drawing.Point(105, 80);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(237, 22);
             this.txtEmail.TabIndex = 3;
@@ -57,37 +59,70 @@ namespace Beadando_sp2p8b
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(72, 140);
+            this.checkBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(32, 166);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.Size = new System.Drawing.Size(325, 21);
             this.checkBox1.TabIndex = 10;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Text = "Elfogadom az adatkezelési szabályzatot.";
+            this.checkBox1.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.LightYellow;
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(56, 249);
+            this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(321, 234);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 33);
             this.button1.TabIndex = 19;
             this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 100);
+            this.label2.BackColor = System.Drawing.Color.LightYellow;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(31, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(68, 27);
             this.label2.TabIndex = 20;
-            this.label2.Text = "label2";
+            this.label2.Text = "Műfaj:";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(105, 97);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Action",
+            "Adventure",
+            "Animation",
+            "Biography",
+            "Comedy",
+            "Crime",
+            "Documentary",
+            "Drama",
+            "Family",
+            "Fantasy",
+            "Film-Noir",
+            "Game-Show",
+            "History",
+            "Horror",
+            "Music",
+            "Musical",
+            "Mystery",
+            "Reality-TV",
+            "Romance",
+            "Sci-Fi",
+            "Short",
+            "Sport",
+            "Talk-Show",
+            "Thriller",
+            "War",
+            "Westend"});
+            this.comboBox1.Location = new System.Drawing.Point(105, 121);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 21;
@@ -95,17 +130,20 @@ namespace Beadando_sp2p8b
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 25);
+            this.label3.BackColor = System.Drawing.Color.LightYellow;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(8, 16);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(420, 42);
             this.label3.TabIndex = 22;
-            this.label3.Text = "label3";
+            this.label3.Text = "Hírlevélre feliratkozás";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 300);
+            this.BackgroundImage = global::Beadando_sp2p8b.Properties.Resources.popcorn_blue_wooden_background_top_view_flapper_numerator_movies_83196_759;
+            this.ClientSize = new System.Drawing.Size(436, 325);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
