eb0ce71d7cffc017eeac45bdd1ed63b66c4cbfab
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-16 22:16:25 +0100

    Form1 Design

--- Beadando_sp2p8b/Beadando_sp2p8b/Form1.Designer.cs
+++ Beadando_sp2p8b/Beadando_sp2p8b/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace Beadando_sp2p8b
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -48,6 +48,7 @@ namespace Beadando_sp2p8b
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightYellow;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 134);
             this.dataGridView1.Name = "dataGridView1";
@@ -58,32 +59,37 @@ namespace Beadando_sp2p8b
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1057, 12);
+            this.button1.BackColor = System.Drawing.Color.LightYellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(872, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(260, 109);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Iratkozz fel, hogy értestést kapj a kedvenc műfajodban megjelenő új filmekről!";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
+            this.chart1.BackColor = System.Drawing.Color.LightYellow;
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
             this.chart1.Location = new System.Drawing.Point(547, 134);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(595, 378);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.LightYellow;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Action",
@@ -112,7 +118,7 @@ namespace Beadando_sp2p8b
             "Thriller",
             "War",
             "Westend"});
-            this.comboBox1.Location = new System.Drawing.Point(75, 90);
+            this.comboBox1.Location = new System.Drawing.Point(95, 95);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 3;
@@ -122,24 +128,30 @@ namespace Beadando_sp2p8b
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightYellow;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(17, 93);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(68, 27);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Genre:";
+            this.label1.Text = "Műfaj:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 12);
+            this.label4.BackColor = System.Drawing.Color.LightYellow;
+            this.label4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(17, 18);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(286, 54);
             this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
+            this.label4.Text = "Filmkereső";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(298, 88);
+            this.numericUpDown1.BackColor = System.Drawing.Color.LightYellow;
+            this.numericUpDown1.Location = new System.Drawing.Point(390, 99);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             2020,
             0,
@@ -163,18 +175,20 @@ namespace Beadando_sp2p8b
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(250, 90);
+            this.label3.BackColor = System.Drawing.Color.LightYellow;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(260, 95);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.Size = new System.Drawing.Size(114, 27);
             this.label3.TabIndex = 11;
-            this.label3.Text = "Year:";
+            this.label3.Text = "Megjelenés:";
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Location = new System.Drawing.Point(907, 12);
+            this.panel2.Location = new System.Drawing.Point(795, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(109, 100);
+            this.panel2.Size = new System.Drawing.Size(71, 60);
             this.panel2.TabIndex = 13;
             // 
             // Form1
